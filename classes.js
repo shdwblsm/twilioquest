30bca692cfbca2f6bba558e6bc42ab3e2c67e674
shdwblsm <Sara.Lovelace@shdwblsm.net>
2020-10-15 21:21:30 -0400

    classes.js

---
+++ classes.js
@@ -0,0 +1,40 @@
+
+class Materializer {
+	
+	constructor(targetName) {
+		this.target = targetName;  
+		this.activated = false;
+	}
+	
+	activate() {
+		this.activated = true;
+		return this.activated;
+	}
+  
+
+	materialize() {
+		
+		if (this.activated === true) {
+			
+			return this.target;
+		
+		} else {
+		
+			return;
+			
+		}
+		
+    }
+ }
+
+ 
+// The following lines of code are not required for the solution, but can be
+// used by you to test your solution.
+const m = new Materializer('Kevin');
+console.log(m.activated); // would print "false"
+
+m.activate();
+console.log(m.activated); // would print "true"
+
+console.log(m.materialize()); // would print "Kevin"
+
