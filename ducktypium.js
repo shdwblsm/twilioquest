4e02fce89fb500e6ac9a9e66ff86257db9eaf5b8
shdwblsm <Sara.Lovelace@shdwblsm.net>
2020-11-07 09:14:11 -0600

    Working refract()

--- ducktypium.js
+++ ducktypium.js
@@ -1,34 +1,78 @@
 class Ducktypium {
+	
+
+	
 	constructor(color) {
-		this.color = color;
-		this.calibrationSequence = [];
-	}
-	
-	refract() { 
-	
-		try {
-			if (this.color === 'red') { console.log('red'); } 
-			if (this.color === 'blue') { console.log('purple'); }
-			if (this.color === 'yellow') { console.log('orange'); }
-			
-			
-	
-		} catch(e) {
+		const validColors = ['yellow', 'red', 'blue'];
 		
-			console.log(this.color + ": Color Selection Incorrect.");
-		
+		if (!validColors.includes(color)) {
+			
+			throw new TypeError("Color must be red, yellow or blue");
+			
 		}
+		
+		this.color = color;
+		this.calibrationSequence = [];	
+		
+		
+
 	}
 	
+	refract(secColor) { 
+		
+		try {
+				
+			if (this.color === secColor) { 
+				return this.color;
+				
+				
+			} 
+			else if (this.color != secColor && this.color && secColor === 'blue' || 'yellow' || 'red') { 
+						
+				try {
+					if (this.color === 'blue' && secColor === 'red' || this.color === 'red' && secColor === 'blue') {return('purple');}
+				
+					if (this.color === 'yellow' && secColor === 'red' || this.color === 'red' && secColor === 'yellow') {return('orange');}
+				
+					if (this.color === 'blue' && secColor === 'yellow' || this.color === 'yellow' && secColor === 'blue') {return('green');}
+					
+					else { throw "is not primary."; }
+				
+				} 
+				catch(error) {
+					//console.log(error);
+					return secColor + " " + error;	
+				}
+			}
 	
-	calibrate() { }
+			} 
+			catch(error) {
+		
+			return('Something went wrong.');
+			
+			}
+			
+	}
+	
+	calibrate() { 
+	
+	// Sort numbers smallest to largest
+	// Multiply each number in the array by 3
+	// Assign the resulting array to the Ducktypium instance's calibrationSequence variable
+	
+	}
 	
 }
 
 // Test
 
-const dt = new Ducktypium('pink');
+const dt = new Ducktypium('red');
 
 console.log(dt.color); // prints 'red'
 
-console.log(dt.refract('pink')); // prints 'purple'
+console.log(dt.refract('turquoise')); // prints 'purple'
+
+
+dt.calibrate([3, 5, 1]);
+
+console.log(dt.calibrationSequence); // prints [3, 9, 15]
